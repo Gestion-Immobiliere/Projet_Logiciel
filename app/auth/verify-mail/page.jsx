69f00741333e1a5277c720e22fc033e10300bc1c
Djibril-Kane <djibrilkanediallo04@gmail.com>
2025-04-23 18:21:03 +0000

    Ajout de fonctionnalites pour les pages de reinitialisation de mdp et de verification d'email

--- app/auth/verify-mail/page.jsx
+++ app/auth/verify-mail/page.jsx
@@ -1,12 +1,35 @@
 'use client';
 
-import { Mail } from 'lucide-react';
+import { Mail, RefreshCw } from 'lucide-react';
 import { useState } from 'react';
 
 export default function VerifyMailPage() {
     const [email, setEmail] = useState("");
-
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+    const [isVerifying, setIsVerifying] = useState(false)
     
+    const verifierMail = async (e) => {
+        e.preventDefault();
+        setIsVerifying(true);
+        setError("");
+        setSuccess("");
+        try {
+            const response = await fetch(`http:.../${email}`);
+            const data = await response.json();
+            if (!response.ok) {
+                setError(data.message);
+                return;
+            }
+            setSuccess("Un message a été envoyé a votre adresse email!");
+        }   
+        catch (error) {
+        setError("Une erreur s'est produite lors de la vérification");
+        } 
+        finally {
+            setIsVerifying(false); 
+        }
+    };
     
     return (
     <div className="flex items-center justify-center min-h-screen">
@@ -20,7 +43,7 @@ export default function VerifyMailPage() {
                 <input 
                     className="border pl-10 pr-4 py-3 border-gray-200 rounded-xl bg-white/90 focus:outline-none focus:ring-2 focus:ring-primary-400 focus:border-transparent shadow-sm transition-all duration-300 hover:border-gray-300 pl-10 p-2  w-full" 
                     placeholder="votre@email.com" 
-                    type="text" 
+                    type="email" 
                     id="email" 
                     name="email"
                     value = {email}
@@ -28,12 +51,25 @@ export default function VerifyMailPage() {
 
                 />
             </div>
-                 <button
+            <div className="relative group">
+                <button 
+                    onClick={verifierMail}
                     className="mt-8 p-2 w-full border border-transparent shadow-lg rounded-xl text-lg font-semibold bg-gradient-to-r from-primary-600 to-primary-500 hover:from-primary-700 hover:to-primary-600 focus:outline-none focus:ring-2 focus:ring-primary-400 focus:ring-offset-2 shadow-lg transition-all duration-300 transform hover:-translate-y-0.5 cursor-pointer" 
                     type="submit">
-                        Verifier
+                    {isVerifying ? ( 
+                    <>
+                        <div className="absolute flex items-center inset-y-0 left-0 pl-3">
+                            <RefreshCw className="animate-spin mr-2"/>
+                        </div>
+                        Verification en cours...
+                    </>
+                    ) : ( 
+                        "Verifier"
+                    )}
                 </button>
-            
+                </div>
+            {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
+            {success && <p className="mt-4 text-green-500 text-center">{success}</p>}    
     </div>
 </div>
   )
