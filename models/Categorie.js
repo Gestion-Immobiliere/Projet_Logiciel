4474ec49e269d5b6a35e92d88e181fccf62b876c
Samuel Atohoun <143291990+SamuelAtohoun@users.noreply.github.com>
2025-04-24 22:25:58 +0000

    Revert "Back end sam"

--- models/Categorie.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-const categorieSchema = new mongoose.Schema({
-  nom: { type: String, required: true, unique: true }
-})
-
-export default mongoose.model("Categorie", categorieSchema)
--- models/Localisation.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-
-const localisationSchema = new mongoose.Schema({
-  ville: { type: String, required: true, unique: true }
-})
-
-export default mongoose.model("Localisation", localisationSchema)
